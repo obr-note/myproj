2a915ef663fd284eab79aa6173285a4ce32c170b
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-22 19:49:30 +0900

    7.4 list11

--- cmd/myproj/main.go
+++ cmd/myproj/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -17,12 +18,39 @@ func main() {
 	}
 	defer db.Close()
 
-	row := db.QueryRow(`SELECT name, age FROM users WHERE id=$1`, 1)
+	rows := db.QueryRow(`SELECT id, name, age FROM users`)
+	var id int64
 	var name string
-	var age int64
-	err = row.Scan(&name, &age)
+	var age myType
+	err = rows.Scan(&id, &name, &age)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(name, age)
+	fmt.Println(id, name, age)
+}
+
+type myType string
+
+func (mt *myType) Scan(src interface{}) error {
+	switch x := src.(type) {
+	case int64:
+		*mt = myType(fmt.Sprint(src))
+	case float64:
+		*mt = myType(fmt.Sprintf("%.2f", src))
+	case bool:
+		*mt = myType(fmt.Sprint(src))
+	case []byte:
+		if len(x) < 10 {
+			*mt = myType(fmt.Sprintf("[% 02X]", x))
+		} else {
+			*mt = myType(fmt.Sprintf("[% 02X...]", x))
+		}
+	case string:
+		*mt = myType(x)
+	case time.Time:
+		*mt = myType(x.Format("2006/01/02 15:04:05"))
+	case nil:
+		*mt = "nil"
+	}
+	return nil
 }
