4a8046c75ce601ae7b669a5a653d2a6345773481
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-23 11:11:34 +0900

    7.6 18 gorp

--- cmd/myproj/main.go
+++ cmd/myproj/main.go
@@ -2,55 +2,29 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/go-gorp/gorp"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	dsn := os.Getenv("DSN")
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	rows := db.QueryRow(`SELECT id, name, age FROM users`)
-	var id int64
-	var name string
-	var age myType
-	err = rows.Scan(&id, &name, &age)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(id, name, age)
+type Comment struct {
+	Id      int64     `db:"id,primarykey,autoincrement"`
+	Name    string    `db:"name,notnull,default:'名無し',size:200"`
+	Text    string    `db:"text,notnull,size:400"`
+	Created time.Time `db:"created,notnull"`
+	Updated time.Time `db:"updated,notnull"`
 }
 
-type myType string
-
-func (mt *myType) Scan(src interface{}) error {
-	switch x := src.(type) {
-	case int64:
-		*mt = myType(fmt.Sprint(src))
-	case float64:
-		*mt = myType(fmt.Sprintf("%.2f", src))
-	case bool:
-		*mt = myType(fmt.Sprint(src))
-	case []byte:
-		if len(x) < 10 {
-			*mt = myType(fmt.Sprintf("[% 02X]", x))
-		} else {
-			*mt = myType(fmt.Sprintf("[% 02X...]", x))
-		}
-	case string:
-		*mt = myType(x)
-	case time.Time:
-		*mt = myType(x.Format("2006/01/02 15:04:05"))
-	case nil:
-		*mt = "nil"
+func main() {
+	dsn := os.Getenv("DSN")
+	db, _ := sql.Open("postgres", dsn)
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	dbmap.AddTableWithName(Comment{}, "comments")
+	err := dbmap.CreateTablesIfNotExists()
+	if err != nil {
+		log.Fatal(err)
 	}
-	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/obr-note/myproj
 go 1.16
 
 require (
+	github.com/go-gorp/gorp v2.2.0+incompatible // indirect
 	github.com/lib/pq v1.10.3 // indirect
 	github.com/rakyll/statik v0.1.7
 )
