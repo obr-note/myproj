c88092056508389a8af0ab743d6bba4a7fd75a05
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-23 12:14:55 +0900

    7.6 26 json

--- cmd/myproj/main.go
+++ cmd/myproj/main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/go-gorp/gorp"
 	"github.com/labstack/echo"
+
+	_ "github.com/lib/pq"
 )
 
+type Comment struct {
+	Id      int64     `db:"id,primarykey,autoincrement"`
+	Name    string    `db:"name,notnull,default:'名無し',size:200"`
+	Text    string    `db:"text,notnull,size:400"`
+	Created time.Time `db:"created,notnull"`
+	Updated time.Time `db:"updated,notnull"`
+}
+
 func main() {
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	dbmap.AddTableWithName(Comment{}, "comments")
+	err = dbmap.CreateTablesIfNotExists()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
+	e.GET("/api/comments", func(c echo.Context) error {
+		var comments []Comment
+		_, err := dbmap.Select(
+			&comments,
+			"SELECT * FROM comments ORDER BY created desc LIMIT 10",
+		)
+		if err != nil {
+			c.Logger().Error("Select: ", err)
+			return c.String(http.StatusBadRequest, "Select: "+err.Error())
+		}
+		return c.JSON(http.StatusOK, comments)
 	})
-	e.Static("/", "static/")
+	// e.Static("/", "static")
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/obr-note/myproj
 go 1.16
 
 require (
-	github.com/go-gorp/gorp v2.2.0+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/go-gorp/gorp v2.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/lib/pq v1.10.3 // indirect
-	github.com/rakyll/statik v0.1.7
+	github.com/lib/pq v1.10.3
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	github.com/poy/onpar v1.1.2 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 )
